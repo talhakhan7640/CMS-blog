aeabfc09b770347330c3bb7adfbea5497f637070
Talha Khan <talhakhan7640@gmail.com>
2022-01-25 20:18:23 +0530

    fixed casing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import React, { useState, useEffect } from 'react'
-import { Layout } from '../Components'
+import { Layout } from '../components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
