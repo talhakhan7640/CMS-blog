e8f481e8eeb15dba9d443a71587741a5c4b02d48
Talha Khan <talhakhan7640@gmail.com>
2022-01-26 13:00:37 +0530

    changed link to NextLink

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Link from 'next/Link';
+import NextLink from 'next/Link';
 import { getCategories } from '../services'
 
 const Categories = () => {
@@ -15,11 +15,11 @@ const Categories = () => {
                 Categories
             </h3>
             {categories.map((category) => (
-                <Link key={category.slug} href={`/category/${category.slug}`}>
+                <NextLink key={category.slug} href={`/category/${category.slug}`}>
                     <span className='cursor-pointer block pb-3 mb-3'>
                         {category.name}
                     </span>
-                </Link>
+                </NextLink>
             ))}
         </div>
     );
